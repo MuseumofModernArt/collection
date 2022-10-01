58a42633174839dfcd6115472155bd160709d4c3
Urs Roesch <github@bun.ch>
2022-10-01 11:41:39 +0200

    SQL: Postgres October 1. 2022 updates

--- MoMA-collection.postgresql.sql
+++ MoMA-collection.postgresql.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82a5fd0c5b864b4570dd07cc24336d84c5b33d4acf80ff0d1e288ac81f22e9b1
-size 50195866
+oid sha256:3e0dfeb683332209d2c49ad0384fab2730215890d297f188162469c09f14efe5
+size 50219507
